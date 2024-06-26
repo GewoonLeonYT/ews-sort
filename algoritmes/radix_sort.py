27c528029ac9f89a9a8bdf086f6ea300b29ee90d
Leon Grutters <gruttersleonbot2@gmail.com>
2024-06-26 13:42:09 +0200

    Add comments and type annotations to radix sort

--- algoritmes/radix_sort.py
+++ algoritmes/radix_sort.py
@@ -1,9 +1,13 @@
-def radix_sort(array: list[int], num_length: int):
-    one_array = []
-    zero_array = []
+def radix_sort(array: list[int], num_length: int) -> list[int]:
+    one_array: list = []
+    zero_array: list = []
 
     for digit in range(num_length):
         for num in array:
+            """
+            Put num in one_array if n-th binary digit is 1,
+            otherwise put it in zero_array
+            """
             if num >> digit & 1:
                 one_array.append(num)
             else:
